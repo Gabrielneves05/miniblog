56db15f2fb388046efe0f8131a9808905bf11dfa
Gabrielneves05 <gabrielneves05@outlook.com>
2025-04-22 01:08:47 -0300

    styles: creating navbar and footer components / styling components

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,5 @@
 import styles from "./About.module.css";
 
-import React from "react";
-
-
 export default function About() {
     return (
         <div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,5 @@
 import styles from "./Home.module.css";
 
-import React from "react";
-
-
 export default function Home() {
     return (
         <div>
