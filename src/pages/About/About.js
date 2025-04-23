6b22876052ec42223478d3fa3e3b6aa462e719fe
Gabrielneves05 <gabrielneves05@outlook.com>
2025-04-23 17:20:48 -0300

    feat: blocking pages that require authentication / about styling

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,14 @@
 import styles from "./About.module.css";
 
+import { Link } from "react-router-dom";
+
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className={styles.about}>
+            <h2>Sobre nosso Mini <span>Blog</span></h2>
+            <p>Projeto que consiste em um blog simples em formato de rede social desenvolvido utilizando React e Firebase.</p>
+
+            <Link href="/posts/create" className="btn">Criar Post</Link>
         </div>
     );
 }
