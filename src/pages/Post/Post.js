664bb5fbc2b078f74dbc1c4b63875334cb278612
Gabrielneves05 <gabrielneves05@outlook.com>
2025-04-29 16:36:16 -0300

    chore: route and individual post page creation

---
+++ src/pages/Post/Post.js
@@ -0,0 +1,13 @@
+import styles from "./Post.module.css";
+
+import { useParams } from "react-router-dom";
+
+export default function Post() {
+    const { id } = useParams();
+
+    return (
+        <div>
+            <h1>Post {id}</h1>
+        </div>
+    );
+}
