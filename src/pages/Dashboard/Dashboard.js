ef2b8de3811160c81d51a39b367fa3cda707a3f0
Gabrielneves05 <gabrielneves05@outlook.com>
2025-05-08 15:09:27 -0300

    feat: added post deletion functionality

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { useAuthValue } from '../../context/AuthContext';
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useDeleteDocument } from '../../hooks/useDeleteDocument';
 
 
 export default function Dashboard() {
@@ -12,9 +13,7 @@ export default function Dashboard() {
 
     const { documents: posts, loading, error } = useFetchDocuments('posts', null, uid);
 
-    const deleteDocument = (id) => {
-
-    }
+    const { deleteDocument } = useDeleteDocument('posts');
 
     if(loading) {
         return <p>Carregando...</p>;
